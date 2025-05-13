aae398b4550729ae76e2ceb096078da8ddadfb8d
Nicolás Matias Garrido Burgos <ni.garridob@duocuc.cl>
2025-05-13 19:30:36 -0400

    Agregar archivos
    Se agrega la carpeta model con la clase Perfume

---
+++ perfulandia-spa/src/main/java/com/perfulandia/cl/perfulandia_spa/model/Perfume.java
@@ -0,0 +1,14 @@
+package com.perfulandia.cl.perfulandia_spa.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+public class Perfume {
+
+    private String nombre;
+    private String marca;
+    private double precio;
+    private int tamaño;
+    private int stock;
+}
