431be5c920adf054dfe6059d2514fea5a21cddfa
Nicolás Matias Garrido Burgos <ni.garridob@duocuc.cl>
2025-05-23 07:52:45 -0400

    Update and rename PerfulandiaSpaInventario.java to PerfulandiaSpaApplication.java

--- Inventario/src/main/java/com/perfulandia/cl/perfulandia_spa/PerfulandiaSpaApplication.java
+++ Inventario/src/main/java/com/perfulandia/cl/perfulandia_spa/PerfulandiaSpaApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PerfulandiaSpaApplicationInventario {
+public class PerfulandiaSpaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PerfulandiaSpaApplication.class, args);
