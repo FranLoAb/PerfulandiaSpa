056c26e00de41a3740eb00a3ba184dae6ea320e5
Nicolás Matias Garrido Burgos <ni.garridob@duocuc.cl>
2025-05-23 07:48:39 -0400

    Update PerfulandiaSpaUsuarios.java

--- GestionUsuarios/src/main/java/com/perfulandia/cl/perfulandia_spa/PerfulandiaSpaUsuarios.java
+++ GestionUsuarios/src/main/java/com/perfulandia/cl/perfulandia_spa/PerfulandiaSpaUsuarios.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PerfulandiaSpaApplication {
+public class PerfulandiaSpaApplicationUsuarios {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PerfulandiaSpaApplication.class, args);
