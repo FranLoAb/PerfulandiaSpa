1a70e4119493bac15a0dd66819baee65b3fe307c
Nicolás Matias Garrido Burgos <ni.garridob@duocuc.cl>
2025-05-23 07:53:15 -0400

    Update and rename PerfulandiaApplicationSoporte.java to PerfulandiaApplication.java

--- TicketSoporte/src/main/java/com/perfulandia/cl/perfulandia/PerfulandiaApplication.java
+++ TicketSoporte/src/main/java/com/perfulandia/cl/perfulandia/PerfulandiaApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PerfulandiaApplicationSoporte {
+public class PerfulandiaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PerfulandiaApplication.class, args);
