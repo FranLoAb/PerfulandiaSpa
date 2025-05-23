475f605baf407440693f3e15a0b8108fb3fc73e3
Nicolás Matias Garrido Burgos <ni.garridob@duocuc.cl>
2025-05-22 21:39:53 -0400

    Update InventarioController.java

--- Inventario/src/main/java/com/perfulandia/cl/perfulandia_spa/controller/InventarioController.java
+++ Inventario/src/main/java/com/perfulandia/cl/perfulandia_spa/controller/InventarioController.java
@@ -1,8 +1,10 @@
-package com.perfulandia.cl.perfulandia_spa.controller;
+package com.perfulandia.cl.perfulandia_spa.Controller;
 
 import com.perfulandia.cl.perfulandia_spa.model.Perfume;
 import com.perfulandia.cl.perfulandia_spa.services.PerfumeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,33 +18,41 @@ public class InventarioController {
     private PerfumeService perfumeService;
 
     @GetMapping
-    public List<Perfume> listaPerfumes(){
-        return perfumeService.getPerfumes();
+        public ResponseEntity<List<Perfume>> listar(){
+        List<Perfume> perfumes = perfumeService.findAll();
+        if (perfumes.isEmpty()){
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.ok(perfumes);
     }
-
     @PostMapping
-    public Perfume agregarPerfume (@RequestBody Perfume perfume){
-        return perfumeService.savePerfume(perfume);
+    public ResponseEntity<Perfume> guardar(@RequestBody Perfume perfume) {
+        if (perfume.getNombre() == null) {
+            return ResponseEntity.badRequest().build();
+        }
+        Perfume perfumeNuevo = perfumeService.save(perfume);
+        return ResponseEntity.status(HttpStatus.CREATED).body(perfumeNuevo);
+    }
+    @GetMapping ("/{id}")
+    public ResponseEntity<Perfume> buscarPorId(@PathVariable Integer id){
+        try{
+            Perfume perfume = perfumeService.findById(id);
+            return ResponseEntity.ok(perfume);
+        } catch (Exception e){
+            return ResponseEntity.notFound().build();
+        }
     }
 
-    @GetMapping("{id}")
-    public Perfume buscarPerfume (@PathVariable int id){
-        return perfumeService.getPerfumeId(id);
+    @DeleteMapping ("/{id}")
+    public ResponseEntity<?> eliminar(@PathVariable Integer id){
+        try{
+            perfumeService.delete(id);
+            return ResponseEntity.noContent().build();
+        } catch (Exception e){
+            return ResponseEntity.notFound().build();
+        }
     }
 
-    @PutMapping("{id}")
-    public Perfume actualizarPerfume(@PathVariable int id, @RequestBody Perfume perfume){
-        return perfumeService.updatePerfume(perfume);
-    }
-    
-    @DeleteMapping("{id}")
-    public  String eliminarPerfume(@PathVariable int id){
-        return perfumeService.deletePerfume(id);
-    }
-
-    @GetMapping("/total")
-    public int totalPerfumesv2(){
-        return perfumeService.totalPerfumesv2();
-    }
 
 }
+
